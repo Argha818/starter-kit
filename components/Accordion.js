3b73cb18f74012fc4ff93360ab94793018a9e0c1
Argha818 <112699194+Argha818@users.noreply.github.com>
2023-10-26 13:33:37 +0530

    Added FAQ

--- components/Accordion.js
+++ components/Accordion.js
@@ -36,15 +36,16 @@ const Accordion = () => {
 
   return (
     <div className="flex flex-col justify-center items-center md:mt-32 md:mx-60 p-10 rounded-xl h-auto py-20 bg-gray-50">
-      {data.map((data) => {
+      {data.map((data,index) => {
         return (
-          <AccordionUI
+          <AccordionUI key={index}
             title={data.question}
             Id={data.id}
-            children={data.answer}
+            
             Index={Index}
             setIndex={setIndex}
-          ></AccordionUI>
+
+          >{data.answer}</AccordionUI>
         );
       })}
     </div>
